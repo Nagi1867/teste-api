ba37d1bd7f289c86d3c79a5dd2faab4cf3b7668e
Gabriel Kiyoto <120345849+Nagi1867@users.noreply.github.com>
2025-07-10 19:54:05 -0300

    preparando a arquitetura de personController

---
+++ src/test/java/br/com/erudio/integrationtests/controller/PersonControllerIntegrationTest.java
@@ -0,0 +1,50 @@
+package br.com.erudio.integrationtests.controller;
+
+import br.com.erudio.config.TestConfigs;
+import br.com.erudio.integrationtests.testcontainers.AbstractIntegrationTest;
+import br.com.erudio.model.Person;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
+import io.restassured.specification.RequestSpecification;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.testcontainers.shaded.com.fasterxml.jackson.databind.DeserializationFeature;
+import org.testcontainers.shaded.com.fasterxml.jackson.databind.ObjectMapper;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+public class PersonControllerIntegrationTest extends AbstractIntegrationTest {
+    private static RequestSpecification specification;
+    private static ObjectMapper objectMapper;
+    private static Person person;
+
+    @BeforeAll
+    public static void setup() {
+        objectMapper = new ObjectMapper();
+        objectMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+
+        specification = new RequestSpecBuilder()
+                .setBasePath("")
+                .setPort(TestConfigs.SERVER_PORT)
+                .addFilter(new RequestLoggingFilter(LogDetail.ALL))
+                .addFilter(new ResponseLoggingFilter(LogDetail.ALL))
+                .build();
+
+        // Given / Arrange
+        person = new Person(
+                "Leandro",
+                "Costa",
+                "leandro@erudio.com.br",
+                "Uberlândia - Minas Gerais - Brasil",
+                "Male");
+    }
+
+    @Test
+    void name() {
+    }
+}
