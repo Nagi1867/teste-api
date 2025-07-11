4c696e0e7f6bb66dffb69779fd6cee3b0a0da69f
Gabriel Kiyoto <120345849+Nagi1867@users.noreply.github.com>
2025-07-10 21:25:44 -0300

    testando o metodo update

--- src/test/java/br/com/erudio/integrationtests/controller/PersonControllerIntegrationTest.java
+++ src/test/java/br/com/erudio/integrationtests/controller/PersonControllerIntegrationTest.java
@@ -1,5 +1,23 @@
 package br.com.erudio.integrationtests.controller;
 
+import static io.restassured.RestAssured.given;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import br.com.erudio.config.TestConfigs;
 import br.com.erudio.integrationtests.testcontainers.AbstractIntegrationTest;
 import br.com.erudio.model.Person;
@@ -8,25 +26,19 @@ import io.restassured.filter.log.LogDetail;
 import io.restassured.filter.log.RequestLoggingFilter;
 import io.restassured.filter.log.ResponseLoggingFilter;
 import io.restassured.specification.RequestSpecification;
-import org.junit.jupiter.api.*;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.testcontainers.shaded.com.fasterxml.jackson.databind.DeserializationFeature;
-import org.testcontainers.shaded.com.fasterxml.jackson.databind.ObjectMapper;
 
-import java.io.IOException;
-
-import static io.restassured.RestAssured.given;
-import static org.junit.jupiter.api.Assertions.*;
-
-@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@TestMethodOrder(OrderAnnotation.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
-public class PersonControllerIntegrationTest extends AbstractIntegrationTest {
+class PersonControllerIntegrationTest extends AbstractIntegrationTest {
+
     private static RequestSpecification specification;
     private static ObjectMapper objectMapper;
     private static Person person;
 
     @BeforeAll
     public static void setup() {
+
+        // Given / Arrange
         objectMapper = new ObjectMapper();
         objectMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
 
@@ -37,7 +49,6 @@ public class PersonControllerIntegrationTest extends AbstractIntegrationTest {
                 .addFilter(new ResponseLoggingFilter(LogDetail.ALL))
                 .build();
 
-        // Given / Arrange
         person = new Person(
                 "Leandro",
                 "Costa",
@@ -48,7 +59,9 @@ public class PersonControllerIntegrationTest extends AbstractIntegrationTest {
 
     @Test
     @Order(1)
-    void integrationTestGivenPersonObject_when_CreateOnePerson_ShouldReturnAPersonObject() throws IOException {
+    @DisplayName("JUnit integration given Person Object when Create one Person should Return a Person Object")
+    void integrationTestGivenPersonObject_when_CreateOnePerson_ShouldReturnAPersonObject() throws JsonMappingException, JsonProcessingException {
+
         var content = given().spec(specification)
                 .contentType(TestConfigs.CONTENT_TYPE_JSON)
                 .body(person)
@@ -65,6 +78,7 @@ public class PersonControllerIntegrationTest extends AbstractIntegrationTest {
         person = createdPerson;
 
         assertNotNull(createdPerson);
+
         assertNotNull(createdPerson.getId());
         assertNotNull(createdPerson.getFirstName());
         assertNotNull(createdPerson.getLastName());
@@ -74,5 +88,49 @@ public class PersonControllerIntegrationTest extends AbstractIntegrationTest {
 
         assertTrue(createdPerson.getId() > 0);
         assertEquals("Leandro", createdPerson.getFirstName());
+        assertEquals("Costa", createdPerson.getLastName());
+        assertEquals("Uberlândia - Minas Gerais - Brasil", createdPerson.getAddress());
+        assertEquals("Male", createdPerson.getGender());
+        assertEquals("leandro@erudio.com.br", createdPerson.getEmail());
+    }
+
+    @Test
+    @Order(2)
+    @DisplayName("JUnit integration given Person Object when Update one Person should Return a Updated Person Object")
+    void integrationTestGivenPersonObject_when_UpdateOnePerson_ShouldReturnAUpdatedPersonObject() throws JsonMappingException, JsonProcessingException {
+
+        person.setFirstName("Leonardo");
+        person.setEmail("leonardo@erudio.com.br");
+
+        var content = given().spec(specification)
+                .contentType(TestConfigs.CONTENT_TYPE_JSON)
+                .body(person)
+                .when()
+                .put()
+                .then()
+                .statusCode(200)
+                .extract()
+                .body()
+                .asString();
+
+        Person updatedPerson = objectMapper.readValue(content, Person.class);
+
+        person = updatedPerson;
+
+        assertNotNull(updatedPerson);
+
+        assertNotNull(updatedPerson.getId());
+        assertNotNull(updatedPerson.getFirstName());
+        assertNotNull(updatedPerson.getLastName());
+        assertNotNull(updatedPerson.getAddress());
+        assertNotNull(updatedPerson.getGender());
+        assertNotNull(updatedPerson.getEmail());
+
+        assertTrue(updatedPerson.getId() > 0);
+        assertEquals("Leonardo", updatedPerson.getFirstName());
+        assertEquals("Costa", updatedPerson.getLastName());
+        assertEquals("Uberlândia - Minas Gerais - Brasil", updatedPerson.getAddress());
+        assertEquals("Male", updatedPerson.getGender());
+        assertEquals("leonardo@erudio.com.br", updatedPerson.getEmail());
     }
 }
